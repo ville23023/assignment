1230da4580da2bcc4ab7637c2b9eed14558ed07a
ville23023 <ville.kylliainen@student.hamk.fi>
2025-04-15 10:13:24 +0300

    fourth commit

--- index.js
+++ index.js
@@ -7,9 +7,78 @@ const players = JSON.parse(playersData);
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+
+//Get all
 app.get('/api/players', (req,res) =>{
-    res.json(players);
+    res.status(200).json(
+        {
+            status:'Succes',
+            results: players.length,
+            data: players
+        }
+    );
 });
+
+//Get one
+app.get('/api/players/:id', (req,res)=>{
+    const id = Number(req.params.id);
+    const player = players.find(player => player.id === id);
+    if(player){
+        res.json(player);
+    }else{
+        res.status(404).json(
+            {
+                msg:"No player with such ID"
+            }
+        )
+    }; 
+});
+
+//Delete one
+app.delete('/api/players/:id', (req,res) =>{
+    const idToRemove = Number(req.params.id);
+    const player = players.find(player => player.id === idToRemove);
+
+    if(player){
+        players = players.filter(player => player.id != idToRemove)
+        res.status(200).json(
+            {
+                msg: `Player with ${idToRemove} removed from the list`
+            }
+        )
+    }else{
+        res.status(404).json(
+            {
+                msg:"No player with such ID"
+            }
+        )
+    };  
+});
+
+//Create
+app.post('/api/players', (req,res) =>{
+    const newId = players[players.length-1].id + 1;
+    const newPlayer = {
+        id: newId,
+        name: req.body.name,
+        position: req.body.position,
+        goals: req.body.goals,
+        isCaptain: req.body.isCaptain
+    }
+    players.push(newPlayer);
+    res.location('http://localhost:3000/api/players/'+ newId);
+    res.status(201).json(
+        {
+            msg:"New player created",
+            player: newPlayer
+        }
+    );
+});
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
