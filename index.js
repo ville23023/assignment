48a10cd17bf0a123d7ed5c0c2cb504606c51a349
ville23023 <ville.kylliainen@student.hamk.fi>
2025-04-15 09:16:51 +0300

    third commit

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const path = require('path');
 
+const fs = require('fs');
+const playersData = fs.readFileSync('./players.json', 'utf-8');
+const players = JSON.parse(playersData);
+
 const app = express();
 
-const players = ("./players.js");
-
-
+app.get('/api/players', (req,res) =>{
+    res.json(players);
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
